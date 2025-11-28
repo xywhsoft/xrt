07b66acedd7ea2d2c09c2f0ec7151dc26e7f7ec2
xLeaves <xywhsoft@qq.com>
2025-11-28 14:09:45 +0800

    更新 linux 编译脚本

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gcc -shared -m64 xrt.c -O2 -s -ffunction-sections -fdata-sections -Wl,--gc-sections -o xrt.so
--- build_test.sh
+++ build_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gcc -m64 test.c xrt.c
-./a.out
+gcc -m64 test.c xrt.c -O2 -s -ffunction-sections -fdata-sections -Wl,--gc-sections -o test
+./test
