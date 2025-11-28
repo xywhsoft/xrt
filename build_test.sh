a621914c90d36635a83bb2ea02c07f62db5c365c
xLeaves <xywhsoft@qq.com>
2025-11-28 14:18:02 +0800

    更新 linux 测试程序编译脚本

--- build_test.sh
+++ build_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gcc -m64 test.c xrt.c -O2 -s -ffunction-sections -fdata-sections -Wl,--gc-sections -o test
-./test
+gcc -m64 test.c xrt.c -O2 -s -ffunction-sections -fdata-sections -Wl,--gc-sections -o xrt_test
+./xrt_test
